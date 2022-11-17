8ddf6969af4be9439c55aa6a3803d24a2ef1ca86
vadeppa <111563462+vadeppa@users.noreply.github.com>
2022-11-17 11:09:59 +0530

    Update settings.py

--- Apollo/settings.py
+++ Apollo/settings.py
@@ -122,9 +122,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS=[BASE_DIR/'static']
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-django_heroku.settings(locals())
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
